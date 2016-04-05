774c90b829137aa420b72d4908741d4f82084243
Xiangfei <yuxf@yahoo-inc.com>
2016-04-04 18:56:07 -0700

    first commit

--- app.js
+++ app.js
@@ -1,50 +1,61 @@
 var express = require('express');
-var webot = require('weixin-robot');
+var wechat = require('./lib/wechat');
 
-var log = require('debug')('webot-werewolf:log');
-var verbose = require('debug')('webot-werewolf:verbose');
+var log = require('debug')('weixin-werewolf');
 
 // 启动服务
 var app = express();
+app.use(express.query());
 
 var config = {
   token: 'feiyuitravel',
   appid: 'wx5d34f7a9cb77d517',
-  encodingAESKey: '5FLR9cvgrcA4XdaUA1KffSAlZ058bqXHVCigcEzcxij'
+  encodingAESKey: '3GyljhUVInXIcHR0KmIXX30sazb3SrgBNnBQ4RSLM5W'
 };
 
-// 建立多个实例，并监听到不同 path ，
-var webot = new webot.Webot();
-
-// 载入webot1的回复规则
-require('./rules')(webot);
-
-// 启动机器人, 接管 web 服务请求
-webot.watch(app, config);
-
-// 如果需要 session 支持，sessionStore 必须放在 watch 之后
-app.use(express.cookieParser());
-// 为了使用 waitRule 功能，需要增加 session 支持
-app.use(express.session({
-  secret: 'abced111',
-  store: new express.session.MemoryStore()
+app.use('/', wechat(config, function (req, res, next) {
+  // 微信输入信息都在req.weixin上
+  var message = req.weixin;
+  if (message.FromUserName === 'diaosi') {
+    // 回复屌丝(普通回复)
+    res.reply('hehe');
+  } else if (message.FromUserName === 'text') {
+    //你也可以这样回复text类型的信息
+    res.reply({
+      content: 'text object',
+      type: 'text'
+    });
+  } else if (message.FromUserName === 'hehe') {
+    // 回复一段音乐
+    res.reply({
+      type: "music",
+      content: {
+        title: "来段音乐吧",
+        description: "一无所有",
+        musicUrl: "http://mp3.com/xx.mp3",
+        hqMusicUrl: "http://mp3.com/xx.mp3",
+        thumbMediaId: "thisThumbMediaId"
+      }
+    });
+  } else {
+    // 回复高富帅(图文回复)
+    res.reply([
+      {
+        title: '你来我家接我吧',
+        description: '这是女神与高富帅之间的对话',
+        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+        url: 'http://nodeapi.cloudfoundry.com/'
+      }
+    ]);
+  }
 }));
-// 在生产环境，你应该将此处的 store 换为某种永久存储。
-// 请参考 http://expressjs.com/2x/guide.html#session-support
 
 // 在环境变量提供的 $PORT 或 3000 端口监听
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
-  log("Listening on %s", port);
+log("Listening on %s", port);
 });
 
 // 微信接口地址只允许服务放在 80 端口
 // 所以需要做一层 proxy
 app.enable('trust proxy');
-
-// 当然，如果你的服务器允许，你也可以直接用 node 来 serve 80 端口
-// app.listen(80);
-
-if(!process.env.DEBUG){
-  console.log("set env variable `DEBUG=webot-werewolf:*` to display debug info.");
-}
